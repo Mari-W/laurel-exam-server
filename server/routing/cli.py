4bf119129fdfae41708b9a16845843ffcd5d358b
Mari <weidner@cs.uni-freiburg.de>
2023-01-12 12:29:12 +0100

    prepare test exam

--- server/routing/cli.py
+++ server/routing/cli.py
@@ -17,7 +17,8 @@ def download():
     cli = render_template("cli", version=Env.get("CLI_VERSION"),
                           auth_url=Env.get("AUTH_URL"), auth_cookie=Env.get("AUTH_COOKIE"),
                           exam_url=Env.get("PUBLIC_URL"), exam_ssh=Env.get("PUBLIC_SSH"),
-                          exam_ssh_port=Env.get("PUBLIC_SSH_PORT"))
+                          exam_ssh_port=Env.get("PUBLIC_SSH_PORT"),
+                          read_only=Env.get_bool("READ_ONLY"))
     r = Response(response=cli, status=200, mimetype="text/python")
     r.headers["Content-Type"] = "text/python; charset=utf-8"
     return r
