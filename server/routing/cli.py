3b21ec2f3578ee676ef8dedd52c1c16af669c7a2
Marius Weidner <weidner@cs.uni-freiburg.de>
2024-06-27 13:37:53 +0200

    update cli

--- server/routing/cli.py
+++ server/routing/cli.py
@@ -6,15 +6,9 @@ from server.routing.decorators import authorized_route
 cli_bp = Blueprint("cli", __name__)
 
 
-@cli_bp.route("/version")
-@authorized_route
-def version():
-    return Env.get("CLI_VERSION"), 200
-
-
 @cli_bp.route("/download")
 def download():
-    cli = render_template("cli", version=Env.get("CLI_VERSION"),
+    cli = render_template("cli",
                           auth_url=Env.get("AUTH_URL"), auth_cookie=Env.get("AUTH_COOKIE"),
                           exam_url=Env.get("PUBLIC_URL"), exam_ssh=Env.get("PUBLIC_SSH"),
                           exam_ssh_port=Env.get("PUBLIC_SSH_PORT"),
